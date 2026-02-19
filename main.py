6bc6d1899f48175489ce2c06197d18b06bacb563
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-02-19 16:53:29 -0300

    feat: focus script on registering all weather data from feburary 9th 2026 until yesterday

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from db_connection import save, load
-from read_weather_api import read_todays_weather, request_past_data
-from datetime import datetime
+from read_weather_api import read_weather_api
+from datetime import datetime, timedelta
 
 
 def main():
     data = load()
-    if (len(data) > 0) and (data[0].date.date() == datetime.now().date()):
-        print("You have already registered today's weather")
-        if input("Do you want to register it again? [y/N]: ").strip().lower() == 'n':
-            return
+    start_date = datetime.fromisoformat("2026-02-09").date() if len(data) == 0 else data[0].date.date()
+    end_date = datetime.now().date() - timedelta(days=1)
+    
+    if len(data) > 0 and start_date >= end_date:
+        print("You have already registered past weather")
+        return
 
     print("Reading api...")
-    weather_data = request_past_data(data[0].date.date())
-    weather_data.append(read_todays_weather())
+    weather_data = read_weather_api(start_date=start_date, end_date=end_date)
 
     print("Saving data in database...")
     save(weather_data)
 
-    print("Today's weather saved successfully")
+    print("Past weather saved successfully")
 
 
 if __name__ == "__main__":
--- read_weather_api.py
+++ read_weather_api.py
@@ -1,5 +1,5 @@
 from weather import Weather
-from datetime import datetime, timedelta
+from datetime import date, datetime
 import os
 import requests
 
@@ -8,34 +8,22 @@ _MAX_TEMP = "temperature_2m_max"
 _HUMIDITY = "relative_humidity_2m_mean"
 
 
-def request_today_data():
-    url = "https://api.open-meteo.com/v1/forecast"
-    params = {
-        "latitude": os.getenv("LATITUDE"),
-        "longitude": os.getenv("LONGITUDE"),
-        "daily": f"{_MIN_TEMP},{_MAX_TEMP},{_HUMIDITY}"
-    }
-
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-
-    data = response.json()
-    return {key: value[0] for key, value in data["daily"].items()}
-
-
-def request_past_data(start_date: datetime):
+def read_weather_api(start_date: date, end_date: date):
+    if start_date == end_date: return []
+    if start_date > end_date: raise ValueError("start date is after end date")
+    
     url = "https://archive-api.open-meteo.com/v1/archive"
     params = {
         "latitude": os.getenv("LATITUDE"),
         "longitude": os.getenv("LONGITUDE"),
         "daily": f"{_MIN_TEMP},{_MAX_TEMP},{_HUMIDITY}",
-        "start_date": start_date + timedelta(days=1),
-        "end_date": datetime.now().date() - timedelta(days=1)
+        "start_date": start_date,
+        "end_date": end_date
     }
-    
+
     response = requests.get(url, params=params)
     response.raise_for_status()
-    
+
     data = response.json()["daily"]
     return [
         Weather(
@@ -45,13 +33,3 @@ def request_past_data(start_date: datetime):
             date=datetime.fromisoformat(data["time"][index])
         ) for index in range(len(data["time"]))
     ]
-
-
-def read_todays_weather() -> Weather:
-    data = request_today_data()
-    return Weather(
-        min=data[_MIN_TEMP],
-        max=data[_MAX_TEMP],
-        air_humidity=data[_HUMIDITY],
-        date=datetime.now()
-    )
