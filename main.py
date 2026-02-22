f7a36649d354ccf3774f5a88374fec5b79fffdd8
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-02-22 15:11:27 -0300

    fix(duplicate entry): avoid duplicating last entry

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 
 def main():
     data = load()
-    start_date = datetime.fromisoformat("2026-02-09").date() if len(data) == 0 else data[0].date.date()
+    start_date = datetime.fromisoformat("2026-02-09").date() if len(data) == 0 else data[0].date.date() + timedelta(days=1)
     end_date = datetime.now().date() - timedelta(days=1)
     
     if len(data) > 0 and start_date >= end_date:
