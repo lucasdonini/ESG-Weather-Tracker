1b2aaa2b00cb54ea8898c6651342c9f80766025e
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-03-03 09:22:35 -0300

    feat(actions): configure workflow to deploy app in every push

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ESG-Weather-Tracker/' // Github repository name to be able to deploy on github pages
 })
